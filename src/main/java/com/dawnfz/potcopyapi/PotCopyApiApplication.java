d5ae63d6ac9a4cf04e6ebbec49233abad1c142f2
ねねだん <admin@dawnfz.com>
2022-11-10 11:07:42 +0800

    更改为使用外部容器

--- src/main/java/com/dawnfz/potcopyapi/PotCopyApiApplication.java
+++ src/main/java/com/dawnfz/potcopyapi/PotCopyApiApplication.java
@@ -2,14 +2,22 @@ package com.dawnfz.potcopyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@ServletComponentScan
 @SpringBootApplication
-public class PotCopyApiApplication
+public class PotCopyApiApplication extends SpringBootServletInitializer
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(PotCopyApiApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(PotCopyApiApplication.class);
+    }
 }
